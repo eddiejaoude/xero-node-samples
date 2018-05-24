2721562d1461ea020a1ba54a471f08e96ca9d1f7
Phil Alsford <phil.alsford@xero.com>
2018-05-25 07:35:06 +1200

    Initial Commit

---
+++ private_example.js
@@ -0,0 +1,19 @@
+const XeroClient = require('xero-node').AccountingAPIClient;
+
+const config = {
+    "appType": "private",
+    "consumerKey": "xxx",
+    "consumerSecret": "xxx",
+    "privateKeyPath": "xxx/privatekey.pem"
+};
+
+(async function() {
+    let xero = new XeroClient(config);
+
+    await xero.contacts.create({
+        Name: 'Phil'
+    })
+
+    const res = await xero.contacts.get();
+    console.log("First Contac: ", res.Contacts[0].Name);
+})();
