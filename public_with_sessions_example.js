34402dc8ac7eddc7d02d15aa75e63f4df29bda06
SidneyAllen <sid.maestre@gmail.com>
2018-06-04 14:12:19 -0700

    Fix tabs

--- public_with_sessions_example.js
+++ public_with_sessions_example.js
@@ -25,37 +25,36 @@ app.get('/connect', async function(req, res) {
 	let xeroClient = new XeroClient(config,req.session);
 
 	try {
-        let requestToken = await xeroClient.oauth1Client.getRequestToken();
-	    let authoriseUrl = xeroClient.oauth1Client.buildAuthoriseUrl(requestToken);
-	    req.session.oauthRequestToken = requestToken;
-	  
-	    res.redirect(authoriseUrl);
-    } catch (err) {
-        res.send("Sorry, something went wrong");
-    }
+		let requestToken = await xeroClient.oauth1Client.getRequestToken();
+		let authoriseUrl = xeroClient.oauth1Client.buildAuthoriseUrl(requestToken);
+		req.session.oauthRequestToken = requestToken;
+		res.redirect(authoriseUrl);
+	} catch (err) {
+		res.send("Sorry, something went wrong");
+	}
 })
 
 app.get('/callback', async function(req, res) {
 	let xeroClient = new XeroClient(config,req.session);
-    let savedRequestToken = req.session.oauthRequestToken;
-    let oauth_verifier = req.query.oauth_verifier;
-    let accessToken = await xeroClient.oauth1Client.swapRequestTokenforAccessToken(savedRequestToken, oauth_verifier);
-    req.session.accessToken = accessToken;
+	let savedRequestToken = req.session.oauthRequestToken;
+	let oauth_verifier = req.query.oauth_verifier;
+	let accessToken = await xeroClient.oauth1Client.swapRequestTokenforAccessToken(savedRequestToken, oauth_verifier);
+	req.session.accessToken = accessToken;
 
-    res.redirect('/organisation');
+	res.redirect('/organisation');
 })
 
 app.get('/organisation', async function(req, res) {
 	let xeroClient = new XeroClient(config,req.session.accessToken);
 	
 	try {
-        let organisations = await xeroClient.organisations.get()
-        res.send("Hello, " + organisations.Organisations[0].Name);
-    } catch (err) {
-       res.send("Sorry, something went wrong");
-    }
+		let organisations = await xeroClient.organisations.get()
+		res.send("Hello, " + organisations.Organisations[0].Name);
+	} catch (err) {
+		res.send("Sorry, something went wrong");
+	}
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Your Xero basic public app is running at localhost:" + app.get('port'))
+	console.log("Your Xero basic public app is running at localhost:" + app.get('port'))
 })
